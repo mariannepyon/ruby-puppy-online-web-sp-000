b720176671b44a81f6f831219d0a06611a569ac3
Marianne Pyon <mariannepyon@gmail.com>
2019-08-15 20:32:20 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -6,16 +6,12 @@ class Dog
 
   def initialize(name)
     @name = name
-    @@all << self
+    save
   end
 
   def save
     @@all << self
   end
-
-  #def self.save
-    #puts dog.name
-  #end??
 
   def self.all
     @@all.each do |dog|
@@ -30,7 +26,6 @@ end
   def self.print_all
     @@all.each do |dog|
       puts dog.name
-
   end
   end
 end
